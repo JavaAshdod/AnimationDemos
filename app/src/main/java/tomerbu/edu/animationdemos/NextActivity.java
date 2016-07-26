9fe79da933c249e52f4f9967c7b6a125c8a629dd
javaashdod <java.ashdod@gmail.com>
2016-07-26 14:20:38 +0300

    card flip animation using object animator

--- app/src/main/java/tomerbu/edu/animationdemos/NextActivity.java
+++ app/src/main/java/tomerbu/edu/animationdemos/NextActivity.java
@@ -1,7 +1,10 @@
 package tomerbu.edu.animationdemos;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NextActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+    }
+
+    public void startAnimation(View view) {
+        //Load the animation:
+        Animator explode = AnimatorInflater.loadAnimator(this, R.animator.explode);
+
+        //Set the target
+        explode.setTarget(view);
+
+        //start the animation
+        explode.start();
     }
 }
